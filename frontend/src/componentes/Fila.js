872f73e5fa42a9f4d0681918d1509106e0d7154e
danielxhalo <danielxhalo@hotmail.com>
2022-05-17 21:11:41 -0300

    CSS FEITO DA PARTE SUPERIOR DO SITE

--- frontend/src/componentes/Fila.js
+++ frontend/src/componentes/Fila.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Fila ({conteudo}){
 return(
-    <ul>
-        {conteudo.map(dados => {return <li>{dados.Mensagem} - {dados.Titulo}- {dados.Data}</li>} )}
+    <ul >
+        {conteudo.map(dados => {return <li >{dados.Mensagem} - {dados.Titulo}- {dados.Data}</li>} )}
     </ul>
 )
 }
